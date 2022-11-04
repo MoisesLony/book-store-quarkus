97f70acf22461594dedd43c5664c633440147e10
Moises Lony <54813324+MoisesLony@users.noreply.github.com>
2022-11-04 00:46:21 -0600

    Initial commit

---
+++ src/main/java/com/Timeout/samples/entities/Book.java
@@ -0,0 +1,82 @@
+package com.Timeout.samples.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "games")
+@NamedQueries({
+        @NamedQuery(name = "Game.findAll", query = "SELECT b FROM Book b")
+})
+
+
+public class Book implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String game;
+    private String genre;
+    private String description;
+
+    private String developer;
+
+    private String publisher;
+    private float price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
